82e1465bf8261e0734011d4effae91678dab8f00
D <dyeshiamonet@gmail.com>
2020-05-12 18:57:56 -0500

    add db script on port 4000

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
 import { getAllEmployees } from './api/index.js'
 
+// IIFE
+(async () => {
+  const employees = await getAllEmployees()
+
+  tbody.innerHTML = employees.map(({ employee_name: name, employee_age: age, employee_salary: salary }) => `
+  <tr>
+  <td>${name}</td>
+  <td>${age}</td>
+  <td>${salary}</td>
+  </tr>
+  `
+  ).join(' ')
+})()
+
 const tbody = document.querySelector('tbody')
-
-getAllEmployees().then(employees => {
-  const employeesHtml = employees.map(({ employee_name: name, employee_age: age, employee_salary: salary }) => {
-    return `
-    <tr>
-      <td>${name}</td>
-      <td>${age}</td>
-      <td>${salary}</td>
-      </tr>
-    `
-  }).join(' ')
-
-  tbody.innerHTML = employeesHtml
-})
