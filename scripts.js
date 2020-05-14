4dfeef8bfbaa5eb55ea09e64292469f825a9c66c
D <dyeshiamonet@gmail.com>
2020-05-14 09:03:52 -0500

    update function

--- scripts.js
+++ scripts.js
@@ -1,16 +1,38 @@
 // Modules in the browser require full paths and extensions
 import { getAllEmployees } from './api/index.js'
 
+const app = document.querySelector('#app')
+
+// Clone the 'document fragment'
+const templateContent = app.querySelector('template').content.cloneNode(true)
+const ths = ['Name', 'Age', 'Salary']
+
+const tbody = templateContent.querySelector('tbody')
+
+// Maintain a reference to this 1️⃣ for adding it back l8r.
+const loader = tbody.querySelector('#loader')
+
+// 'thead tr' starts off empty
+templateContent.querySelector('thead tr').innerHTML = ths
+  .map((th) => `<th>${th}</th>`)
+  .join(' ')
+
+// Add all of the elements to '#app'
+app.appendChild(templateContent);
+
+// Use IIFE as we definitely want to TRY to 'initialize' our app with the data
 (async () => {
   try {
     const employees = await getAllEmployees()
 
-    loader.classList.toggle('loader')
-
-    tbody.innerHTML += employees
+    /**
+     * Just replace the contents of 'tbody' outright.
+     * We have a reference to 'loader.'
+     */
+    tbody.innerHTML = employees
       .map(
         ({ employee_name: name, employee_age: age, employee_salary: salary }) => `
-          <tr>
+          <tr class="flex flex--justify-content-space-between">
             <td>${name}</td>
             <td>${age}</td>
             <td>${salary}</td>
@@ -26,16 +48,3 @@ import { getAllEmployees } from './api/index.js'
     `
   }
 })()
-
-const app = document.querySelector('#app')
-const templateContent = app.querySelector('template').content.cloneNode(true)
-
-const tbody = templateContent.querySelector('tbody')
-const loader = tbody.querySelector('#loader')
-const ths = ['name', 'Age', 'Salary']
-
-templateContent.querySelector('thead tr').innerHTML = ths
-  .map((th) => `<th>${th}</th>`)
-  .join(' ')
-
-app.appendChild(templateContent)
